3cc3f389fbe593c97a11f1f9f38bef27478a3764
Alejandro Palacio <alejandrooss94@gmail.com>
2023-10-31 17:21:31 -0500

    Se hace configuracion inicial del microservicio student

--- service-student/src/main/java/com/microservice/student/ServiceStudentApplication.java
+++ service-student/src/main/java/com/microservice/student/ServiceStudentApplication.java
@@ -2,7 +2,9 @@ package com.microservice.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient // Para decir que es un cliente de EUREKA
 @SpringBootApplication
 public class ServiceStudentApplication {
 
