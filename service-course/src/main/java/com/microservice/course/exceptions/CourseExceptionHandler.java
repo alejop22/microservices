3138f60ce6b580d21e2456d0ef24c63e48ae8e2c
Alejandro Palacio <alejandrooss94@gmail.com>
2023-11-01 17:45:25 -0500

    Se corrige error en el exception handler

--- service-course/src/main/java/com/microservice/course/exceptions/CourseExceptionHandler.java
+++ service-course/src/main/java/com/microservice/course/exceptions/CourseExceptionHandler.java
@@ -2,12 +2,14 @@ package com.microservice.course.exceptions;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class CourseExceptionHandler {
+    @ExceptionHandler(CourseExceptions.class)
     public ResponseEntity<?> handleException(CourseExceptions ex) {
         Map<String, String> responseErrorBody = new HashMap<>();
         responseErrorBody.put("message", ex.getMessage());
