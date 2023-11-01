be41f8f91395fb427904ac595be8a8ba438723fb
Alejandro Palacio <alejandrooss94@gmail.com>
2023-11-01 14:24:29 -0500

    Se agrega capa basica de controllador para student

---
+++ service-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.microservice.student.controller;
+
+import com.microservice.student.persistence.entity.Student;
+import com.microservice.student.service.IStudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    private final IStudentService studentService;
+
+    public StudentController(IStudentService studentService) {
+        this.studentService = studentService;
+    }
+    @PostMapping("/create")
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.studentService.save(student));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> findAllStudents() {
+        return ResponseEntity.ok(this.studentService.findAll());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<Student> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.studentService.findById(id));
+    }
+
+}
